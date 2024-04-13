a44901c0367187f4d25e192008e80ff13d5988d2
Dulat Rakhymkul <dulat.rkul03@gmail.com>
2024-04-13 18:31:25 +0500

    adapter

--- Cashback/views.py
+++ Cashback/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import requests
+import json
+from django.http import JsonResponse
 
-# Create your views here.
+
+# Create your views here.'
+def run_page(request):
+    return render(request, 'main.html')
